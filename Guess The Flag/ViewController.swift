bdf3030087cb8a4b3cfe0ed10d340fb96b6e1add
Jimmy Gutierrez <jigutierrez@oceanreef.com>
2019-05-26 13:35:34 -0400

    Guess The Flag project 2

---
+++ Guess The Flag/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by Jimmy Gutierrez on 5/26/19.
+//  Copyright © 2019 JimmyG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        askQuestion()
+        addBorder()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    func addBorder() {
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+    }
+    
+    func borderColor() {
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+        scoreLabel.text = "Your Score is \(score)"
+        
+    }
+    
+    
+
+
+}
+
